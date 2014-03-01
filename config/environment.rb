a088e5c6a133fa9a101a714d5c85364437938170
Gerard Beugelink <gerard.beugelink@gmail.com>
2014-03-01 15:23:35 +0100

    Start fresh app

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Trimsalon01::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trimsalon01::Application.config.secret_key_base = '8dcd7148cc3a2b8f57d86f5bef16cb5254c26226cae899a6750e865698f432cc5392cb87c64d58366e40b07bb69723a1d89556f6ca8396137932423da5b125dd'
